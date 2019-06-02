cd2ba1489f71d7a604d0a14cbf355a91a9132eb8
Siddhant Sinha <sid.sinha94@gmail.com>
2019-06-02 23:20:20 +0530

    cleanup

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"shiriff/cmd"
-	"shiriff/config"
 )
 
 func main() {
-	// fmt.Println(cmd.SayHello())
-	fmt.Printf("%+v",config.GetConfig())
-	fmt.Printf("\n")
 	cmd.SetCommands()
 	cmd.StartApp()
 }
